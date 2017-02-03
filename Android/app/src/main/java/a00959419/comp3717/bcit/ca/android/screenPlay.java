d8872b1530180af379cb9211d1fb0b79ab7247e6
Andrew Busto <aBusto@shaw.ca>
2017-02-02 19:38:37 -0800

    kind of have the hold-down button thing working.  super janky, though.  Don't really want to do anything more with it until we talk to our instructor :s

--- Android/app/src/main/java/a00959419/comp3717/bcit/ca/android/screenPlay.java
+++ Android/app/src/main/java/a00959419/comp3717/bcit/ca/android/screenPlay.java
@@ -3,21 +3,50 @@ package a00959419.comp3717.bcit.ca.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 
 /**
  * Created by Kunlaya on 2017-01-26.
  */
 
 public class screenPlay extends Activity{
+    private move mov = new move();
+    private Thread thread = new Thread(mov);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        findViewById(R.id.buttonRight).setOnTouchListener(new pressListener());
     }
 
     public void buttonPauseClick(View view) {
         Intent paused = new Intent(screenPlay.this, screenPaused.class);
         startActivity(paused);
+    }
+
+    class pressListener implements OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch(event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    thread.start();
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    thread.interrupt();
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    class move implements Runnable {
+        public void run() {
+            while(!Thread.interrupted()) {
+                findViewById(R.id.tempHouse).offsetLeftAndRight((int)(Math.random() * 3) - 1);
+            }
+        }
     }
 }
