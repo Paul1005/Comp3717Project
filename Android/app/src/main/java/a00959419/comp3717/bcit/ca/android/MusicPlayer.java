07550a4c9871197c67354faa323f0b1ecf94bf5b
Paul <mccarliep@gmail.com>
2017-02-09 19:41:36 -0800

    music player extends service

--- Android/app/src/main/java/a00959419/comp3717/bcit/ca/android/MusicPlayer.java
+++ Android/app/src/main/java/a00959419/comp3717/bcit/ca/android/MusicPlayer.java
@@ -1,8 +1,19 @@
 package a00959419.comp3717.bcit.ca.android;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Paul on 2017-02-09.
  */
 
-public class MusicPlayer {
+public class MusicPlayer extends Service{
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
