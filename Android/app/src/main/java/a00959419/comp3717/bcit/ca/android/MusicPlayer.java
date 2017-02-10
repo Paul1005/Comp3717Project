5b437aa4e9215d7b34c6c31da56ef36a2ca7fc0c
Paul <mccarliep@gmail.com>
2017-02-10 08:46:03 -0800

    made intentservice

--- Android/app/src/main/java/a00959419/comp3717/bcit/ca/android/MusicPlayer.java
+++ Android/app/src/main/java/a00959419/comp3717/bcit/ca/android/MusicPlayer.java
@@ -1,7 +1,9 @@
 package a00959419.comp3717.bcit.ca.android;
 
+import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
@@ -9,11 +11,19 @@ import android.support.annotation.Nullable;
  * Created by Paul on 2017-02-09.
  */
 
-public class MusicPlayer extends Service{
+public class MusicPlayer extends IntentService{
 
-    @Nullable
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public MusicPlayer(String name) {
+        super(name);
+    }
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    protected void onHandleIntent(Intent intent) {
+
     }
 }
