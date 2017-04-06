79e674cd6da0357eda54858f2181b4ada35a3a1e
kunlaya <kunlayajwry@hotmail.com>
2017-04-06 11:15:57 -0700

    ok

--- Android/app/src/main/java/a00959419/comp3717/bcit/ca/android/ScreenSettings.java
+++ Android/app/src/main/java/a00959419/comp3717/bcit/ca/android/ScreenSettings.java
@@ -2,12 +2,17 @@ package a00959419.comp3717.bcit.ca.android;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.media.AudioManager;
+
+import com.esri.core.internal.tasks.ags.v;
+
 import static a00959419.comp3717.bcit.ca.android.ScreenMain.mediaPlayer;
 import static a00959419.comp3717.bcit.ca.android.ScreenMain.soundFX;
 
@@ -19,17 +24,21 @@ public class ScreenSettings extends Activity {
     private SeekBar volumeSeekbar = null;
     private AudioManager audioManager = null;
     public static boolean mute = false;
+    public static MediaPlayer mp;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
         setContentView(R.layout.activity_settings);
+
+
+
         controlVolume();
-        controlVolume2();
-        controlVolume3();
+       // controlVolume2();
+        //controlVolume3();
     }
 
-    public void onMute(View view) {
+    /*public void onMute(View view) {
         soundFX.start();
         Switch onOffSwitch = (Switch)view;
         if(onOffSwitch.isChecked()){
@@ -39,13 +48,11 @@ public class ScreenSettings extends Activity {
             mediaPlayer.start();
             mute = false;
         }
-    }
+    } */
 
-    private void controlVolume()
-    {
-        try
-        {
-            volumeSeekbar = (SeekBar)findViewById(R.id.seekBar1);
+    private void controlVolume() {
+        try {
+            volumeSeekbar = (SeekBar) findViewById(R.id.seekBar1);
             audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
             volumeSeekbar.setMax(audioManager
                     .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
@@ -53,33 +60,28 @@ public class ScreenSettings extends Activity {
                     .getStreamVolume(AudioManager.STREAM_MUSIC));
 
 
-            volumeSeekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
-            {
+            volumeSeekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
                 @Override
-                public void onStopTrackingTouch(SeekBar arg0)
-                {
+                public void onStopTrackingTouch(SeekBar arg0) {
                 }
 
                 @Override
-                public void onStartTrackingTouch(SeekBar arg0)
-                {
+                public void onStartTrackingTouch(SeekBar arg0) {
                 }
 
                 @Override
-                public void onProgressChanged(SeekBar arg0, int progress, boolean arg2)
-                {
+                public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
                     audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
                             progress, 0);
                 }
 
             });
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    private void controlVolume2()
+
+   /* private void controlVolume2()
     {
         try
         {
@@ -154,7 +156,7 @@ public class ScreenSettings extends Activity {
         {
             e.printStackTrace();
         }
-    }
+    } */
 
 
     public void buttonBackClick(View view) {
